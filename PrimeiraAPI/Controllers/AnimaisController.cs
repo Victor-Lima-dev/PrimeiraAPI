8accfd034f093217e54ae738e8244ef00ec83296
VictorLima <victoraugustolima1@gmail.com>
2022-12-08 20:56:54 -0300

    build animais e remedios controller

---
+++ PrimeiraAPI/Controllers/AnimaisController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PrimeiraAPI.Context;
+using PrimeiraAPI.Models;
+
+namespace PrimeiraAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AnimaisController : ControllerBase
+    {
+        //acessar banco de dados somente leitura
+        private readonly AppDbContext _context;
+        //construtuor
+        public AnimaisController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Animal>> Get()
+        {
+            //lista de animais
+            var animais = _context.Animais.ToList();
+
+
+            //verificar se Animal é null
+            if (animais == null)
+            {
+                return NotFound();
+            }
+            return animais.ToList();
+        }
+
+        //metodo para retornar um animal por id
+        [HttpGet("{id:int}", Name = "ObterAnimal")]
+        public ActionResult<Animal> Get(int id)
+        {
+            var animal = _context.Animais.FirstOrDefault(x => x.AnimalId == id);
+
+            //verificar se Animal é null
+            if (animal == null)
+            {
+                return NotFound("Produto nao encontrado");
+            }
+            //retornar o animal pelo id
+            return animal;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Animal animal)
+        {
+            if (animal is null)
+            {
+                BadRequest();
+            }
+
+            _context.Animais.Add(animal);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterAnimal", new { id = animal.AnimalId }, animal);
+
+        }
+
+
+
+        //template de rota /animais/id
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, Animal animal)
+        {
+            if (id != animal.AnimalId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(animal).State = EntityState.Modified;
+            _context.SaveChanges();
+            
+            return Ok(animal);
+        }
+
+        //metodo para excluir um animal
+        [HttpDelete("{id:int}")]
+        public ActionResult<Animal> Delete(int id)
+        {
+            var animal = _context.Animais.FirstOrDefault(x => x.AnimalId == id);
+
+            if (animal == null)
+            {
+                return NotFound();
+            }
+
+            _context.Animais.Remove(animal);
+            _context.SaveChanges();
+
+            return Ok(animal);
+        }
+    }
+   
+
+    }
+  
---
+++ PrimeiraAPI/Controllers/RemediosController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PrimeiraAPI.Context;
+using PrimeiraAPI.Models;
+
+namespace PrimeiraAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RemediosController : ControllerBase
+    {
+        
+        private readonly AppDbContext _context;
+        public RemediosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Remedio>> Get()
+        {
+            var remedios = _context.Remedios.ToList();
+
+            if (remedios == null)
+            {
+                return NotFound();
+            }
+            return remedios.ToList();
+        }
+
+        [HttpGet("{id:int}", Name = "ObterRemedio")]
+        public ActionResult<Remedio> Get(int id)
+        {
+            var remedio = _context.Remedios.FirstOrDefault(x => x.RemedioId == id);
+
+            if (remedio == null)
+            {
+                return NotFound("Produto nao encontrado");
+            }
+            return remedio;
+        }
+
+        [HttpPost]
+
+        public ActionResult Post(Remedio remedio)
+        {
+            if (remedio is null)
+            {
+                BadRequest();
+            }
+
+            _context.Remedios.Add(remedio);
+            _context.SaveChanges();
+            return new CreatedAtRouteResult("ObterRemedio", new { id = remedio.RemedioId }, remedio);
+        }
+
+        [HttpPut("{id:int}")]
+
+        public ActionResult Put(int id, Remedio remedio)
+        {
+            if (id != remedio.RemedioId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(remedio).State = EntityState.Modified;
+            _context.SaveChanges();
+            return Ok(remedio);
+        }
+
+        [HttpDelete("{id:int}")]
+
+        public ActionResult<Remedio> Delete(int id)
+        {
+            var remedio = _context.Remedios.FirstOrDefault(x => x.RemedioId == id);
+
+            if (remedio == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remedios.Remove(remedio);
+            _context.SaveChanges();
+            return Ok(remedio);
+        }
+        
+        
+    }
+}
